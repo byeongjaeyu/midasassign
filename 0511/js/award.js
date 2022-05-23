c33649692f016bdf4fd47cef703b1498d85b588e
유 병재 <ybj1217@midasin.com>
2022-05-23 14:11:53 +0900

    FIX beverage.js 피드백1

--- 0511/js/award.js
+++ 0511/js/award.js
@@ -1,161 +1,160 @@
-const json = {
-    data : [
-        {
-            date : '2019.11',
-            content : {
-                "title" : "고용 노동부 주관, 일·생활 균형 우수 기업 선정",
-                "filepath" : "https://www.ediya.com/files/award/IMG_1580975213558.jpg",
-                "description" : "워라벨 우수 기업이 됐습니다.",
-                "date" : "11"
+{
+    const json = {
+        data : [
+            {
+                date : '2019.11',
+                content : {
+                    "title" : "고용 노동부 주관, 일·생활 균형 우수 기업 선정",
+                    "filepath" : "https://www.ediya.com/files/award/IMG_1580975213558.jpg",
+                    "description" : "워라벨 우수 기업이 됐습니다.",
+                    "date" : "11"
+                }
+            },
+            {
+                date : '2019.10',
+                content : {
+                    "title" : "커피 산업 공로상 농림축산식품부 장관상 수상",
+                    "filepath" : "https://www.ediya.com/files/award/IMG_1580975213558.jpg",
+                    "description" : "상을 탔습니다.",
+                    "date" : "10"
+                }
+            },
+            {
+                date : '2019.06',
+                content : {
+                    "title" : "이디야커피 대한민국 창업대상 국무총리상 수상",
+                    "filepath" : "https://www.ediya.com/files/award/IMG_1580975213558.jpg",
+                    "description" : "서울경제에서 주최하는 대한민국 창업대상 CEO 부문에 국무총리상을 수상하였습니다.",
+                    "date" : "06"
+                }
+            },
+            {
+                date : '2018.12',
+                content : {
+                    "title" : "소비자중심경영(CCM) 인증 획득",
+                    "filepath" : "https://www.ediya.com/files/award/IMG_1580975213558.jpg",
+                    "description" : "소비자 중심 경영 인증했습니다.",
+                    "date" : "12"
+                }
+            },
+            {
+                date : '2017.06',
+                content : {
+                    "title" : "대한민국 창업대상 산업통상자원부 장관상 수상",
+                    "filepath" : "https://www.ediya.com/files/award/IMG_1580975213558.jpg",
+                    "description" : "장관상을 탔습니다.",
+                    "date" : "06"
+                }
+            },
+            {
+                date : '2017.02',
+                content : {
+                    "title" : "사회보험 사각지대해소 '특별상' 수상",
+                    "filepath" : "https://www.ediya.com/files/award/IMG_1580975213558.jpg",
+                    "description" : "특별상을 탔습니다.",
+                    "date" : "02"
+                }
             }
-        },
-        {
-            date : '2019.10',
-            content : {
-                "title" : "커피 산업 공로상 농림축산식품부 장관상 수상",
-                "filepath" : "https://www.ediya.com/files/award/IMG_1580975213558.jpg",
-                "description" : "상을 탔습니다.",
-                "date" : "10"
-            }
-        },
-        {
-            date : '2019.06',
-            content : {
-                "title" : "이디야커피 대한민국 창업대상 국무총리상 수상",
-                "filepath" : "https://www.ediya.com/files/award/IMG_1580975213558.jpg",
-                "description" : "서울경제에서 주최하는 대한민국 창업대상 CEO 부문에 국무총리상을 수상하였습니다.",
-                "date" : "06"
-            }
-        },
-        {
-            date : '2018.12',
-            content : {
-                "title" : "소비자중심경영(CCM) 인증 획득",
-                "filepath" : "https://www.ediya.com/files/award/IMG_1580975213558.jpg",
-                "description" : "소비자 중심 경영 인증했습니다.",
-                "date" : "12"
-            }
-        },
-        {
-            date : '2017.06',
-            content : {
-                "title" : "대한민국 창업대상 산업통상자원부 장관상 수상",
-                "filepath" : "https://www.ediya.com/files/award/IMG_1580975213558.jpg",
-                "description" : "장관상을 탔습니다.",
-                "date" : "06"
-            }
-        },
-        {
-            date : '2017.02',
-            content : {
-                "title" : "사회보험 사각지대해소 '특별상' 수상",
-                "filepath" : "https://www.ediya.com/files/award/IMG_1580975213558.jpg",
-                "description" : "특별상을 탔습니다.",
-                "date" : "02"
-            }
-        }
-    ]
-}
-
-const awards = json.data;
-const award_container = document.getElementById("awards");
-let now_year;
-
-//document fragment
-//이미지 포맷 웹에서
-
-const createYear = (container) => {
-    let box = `
-        <div class="award" id=${now_year}>
-            <p class="award_year">
-                ${now_year}
-            </p>
-            <img
-                class="dot"
-                src=\"../image/dot.png"
-            />
-            <div id=${now_year + "content"}>
-                ${container}
-            </div>
-        </div>
-    `
-    award_container.innerHTML += box;
-}
-
-
-awards.forEach(function(award,index){
-    console.log(index);
-    var date = award.date.split('.');
-    var current = false;
-    if(!now_year || now_year!==date[0]){
-        now_year = date[0];
-    }else{
-        current = true;
+        ]
     }
 
-    let container = `
-        <div class="container">
-            <div class="award_content">
-                <p class="award_content_date">
-                    ${award.content.date}
-                </p>
-                <p class="award_content_title">
-                    ${award.content.title}
-                </p>
-                <img 
-                    class="award_content_scroll" 
-                    id="award_content_scroll"
-                    src=\"../image/arrow_close.png"
-                    width="10px"
-                    height="10px"
-                />
-            </div>
+    const awards = json.data;
+    const award_container = document.getElementById("awards");
+    let now_year;
 
-            <div class="award_detail" id="award_detail">
-                <img
-                    src=${award.content.filepath}
-                    width="auto"
-                    height="100px"
-                />
-                <p>
-                    ${award.content.description}
+    //document fragment
+    //이미지 포맷 웹에서
+
+    const createYear = (container) => {
+        let box = `
+            <div class="award" id=${now_year}>
+                <p class="award_year">
+                    ${now_year}
                 </p>
-                <div class="detail_date">
-                    <p class="detail_date_title">
-                        수상일
-                    </p>
-                    <p class="detail_date_date">
-                        ${award.date + '.' + award.content.date}
-                    </p>
+                <img
+                    class="dot"
+                    src=\"../image/dot.png"
+                />
+                <div id=${now_year + "content"}>
+                    ${container}
                 </div>
             </div>
-        </div>
-    `
-
-    if(!current){
-        createYear(container);
-    }else{
-        const content_container = document.getElementById(now_year+"content");
-        content_container.innerHTML+=container;
+        `
+        award_container.innerHTML += box;
     }
-})
 
 
-for (let i = 0; i < awards.length; i++) {
-    let detail = document.querySelectorAll("#award_detail")[i];
-    let scroll = document.querySelectorAll("#award_content_scroll")[i];
-    console.log(scroll, detail)
-    scroll.addEventListener("click", () => {
-        if(detail.classList.contains("block")){
-            detail.classList.remove("block")
-            scroll.src = '../image/arrow_close.png';
+    awards.forEach(function(award,index){
+        console.log(index);
+        var date = award.date.split('.');
+        var current = false;
+        if(!now_year || now_year!==date[0]){
+            now_year = date[0];
         }else{
-            detail.className += " block"
-            scroll.src = '../image/arrow_open.png'
+            current = true;
+        }
+
+        let container = `
+            <div class="container">
+                <div class="award_content">
+                    <p class="award_content_date">
+                        ${award.content.date}
+                    </p>
+                    <p class="award_content_title">
+                        ${award.content.title}
+                    </p>
+                    <img 
+                        class="award_content_scroll" 
+                        id="award_content_scroll"
+                        src=\"../image/arrow_close.png"
+                        width="10px"
+                        height="10px"
+                    />
+                </div>
+
+                <div class="award_detail" id="award_detail">
+                    <img
+                        src=${award.content.filepath}
+                        width="auto"
+                        height="100px"
+                    />
+                    <p>
+                        ${award.content.description}
+                    </p>
+                    <div class="detail_date">
+                        <p class="detail_date_title">
+                            수상일
+                        </p>
+                        <p class="detail_date_date">
+                            ${award.date + '.' + award.content.date}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        `
+
+        if(!current){
+            createYear(container);
+        }else{
+            const content_container = document.getElementById(now_year+"content");
+            content_container.innerHTML+=container;
         }
     })
-}
 
+
+    for (let i = 0; i < awards.length; i++) {
+        let detail = document.querySelectorAll("#award_detail")[i];
+        let scroll = document.querySelectorAll("#award_content_scroll")[i];
+        console.log(scroll, detail)
+        scroll.addEventListener("click", () => {
+            if(detail.classList.contains("block")){
+                detail.classList.remove("block")
+                scroll.src = '../image/arrow_close.png';
+            }else{
+                detail.className += " block"
+                scroll.src = '../image/arrow_open.png'
+            }
+        })
+    }
+}
 //details태그
-//dot -> css
-//append -> innerhtml
--- 0511/js/beverage.js
+++ 0511/js/beverage.js
@@ -1,126 +1,153 @@
-let drinks = []
-let curIdx = 1;
-let lastIdx;
-let size = 10;
-let loading = false;
+{
+    let currentIndex = 1;
+    let lastIndex;
+    let size = 10;
+    let loading = true
 
-const container = document.getElementById("drinks-container");
-const gotoTop = document.getElementById("gototop");
+    const container = document.getElementById("drinks-container");
+    const gotoTop = document.getElementById("gototop");
 
-const modal = document.getElementById("modal");
-const modalOuter = document.getElementById("modal-outer");
-const modalExit = document.getElementById("modal-exit");
-const modalTitleKr = document.getElementById("modal-title-kr");
-const modalTitleEn = document.getElementById("modal-title-en");
-const modalDescription = document.getElementById("modal-head-description");
-const cal = document.getElementById("cal");
-const sugar = document.getElementById("sugar");
-const protein = document.getElementById("protein");
-const fat = document.getElementById("fat");
-const na = document.getElementById("na");
-const caffeine = document.getElementById("caffeine");
-const body = document.querySelector("body");
+    const modal = document.getElementById("modal");
+    const modalOuter = document.getElementById("modal-outer");
+    const modalExit = document.getElementById("modal-exit");
+    const modalTitleKr = document.getElementById("modal-title-kr");
+    const modalTitleEn = document.getElementById("modal-title-en");
+    const modalDescription = document.getElementById("modal-head-description");
+    const cal = document.getElementById("cal");
+    const sugar = document.getElementById("sugar");
+    const protein = document.getElementById("protein");
+    const fat = document.getElementById("fat");
+    const na = document.getElementById("na");
+    const caffeine = document.getElementById("caffeine");
+    const body = document.querySelector("body");
+    const loadOuter = document.getElementById("onload-outer");
 
+    const showLoading = () => {
+        loadOuter.className += " block";
+        body.className + " onload";
+    }
 
-axios.get('http://localhost:6120/api/drink/list/',{
-    params:{
-        start:1,
-        size:size,
-    },
-}).then(res=>{
-    drinks = drinks.concat(res.data.drink);
-    setDrink(0,size-1)
-    curIdx = res.data.paging.currentIndex;
-    lastIdx = res.data.paging.lastIndex;
-})
+    const closeLoading = () => {
+        loadOuter.classList.remove("block");
+        body.classList.remove("onload");
+    }
 
-
-gotoTop.addEventListener("click",()=>{
-    window.scrollTo(0,0);
-})
-
-modalExit.addEventListener("click",()=>{
-    closeModal()
-})
-
-window.addEventListener("scroll",(e)=>{
-    if((window.innerHeight + window.scrollY) >= document.body.offsetHeight*0.99) {
-        if(curIdx+1>=lastIdx){
-            return
-        }
-        if(loading===false){
-            loading = true
-            axios.get('http://localhost:6120/api/drink/list/',{
-                params:{
-                    start:curIdx,
-                    size:size,
-                },
-            }).then(res=>{
-                console.log(res.data)
-                drinks = drinks.concat(res.data.drink);
-                setDrink(curIdx-1,size-1)
-                curIdx = res.data.paging.currentIndex;
-                lastIdx = res.data.paging.lastIndex;
-            })
+    const showModal = () => {
+        if(!modal.classList.contains("block")){
+            modal.className += " block";
+            modalOuter.className += " block";
+            body.className += "onmodal";
+            closeLoading();
         }
     }
-})
 
-document.addEventListener("click",(e)=>{
-    console.log('!')
-    if(!modal.classList.contains("block") && e.target.classList.contains("drink")){
-        axios.get('http://localhost:6120/api/drink/detail/',{
-                params:{
-                    sn:Number(e.target.id)+1
-                },
-            }).then(res=>{
-                writeModal(res.data.detail);
-            })
+    const closeModal = () => {
+        modal.classList.remove("block");
+        modalOuter.classList.remove("block");
+        body.classList.remove("onmodal");
     }
 
-    if(modal.classList.contains("block") && !modal.contains(e.target)){
-        closeModal();
+    showLoading();
+    axios.get('http://localhost:6120/api/drink/list/',{
+        params:{
+            start:1,
+            size:size,
+        },
+    }).then(res=>{
+        console.log('초기로딩');
+        loading = false;
+        setDrink(res.data.drink);
+        currentIndex = res.data.paging.currentIndex;
+        lastIndex = res.data.paging.lastIndex;
+    })
+
+
+    gotoTop.addEventListener("click",()=>{
+        window.scrollTo(0,0);
+    })
+
+    modalExit.addEventListener("click",()=>{
+        closeModal()
+    })
+
+    window.addEventListener("scroll",()=>{
+        if((window.innerHeight + window.scrollY) >= document.body.offsetHeight*0.99) {
+            if(currentIndex+1>=lastIndex){
+                return
+            }
+            if(loading===false){
+                loading = true
+                showLoading();
+                console.log(currentIndex);
+                axios.get('http://localhost:6120/api/drink/list/',{
+                    params:{
+                        start:currentIndex,
+                        size:size,
+                    },
+                }).then(res=>{
+                    console.log('스크롤로딩');
+                    setDrink(res.data.drink)
+                    currentIndex = res.data.paging.currentIndex;
+                    lastIndex = res.data.paging.lastIndex;
+                })
+            }
+        }
+    })
+
+    document.addEventListener("click",(e)=>{
+        if(!modal.classList.contains("block") && e.target.classList.contains("drink")){
+            showLoading();
+            axios.get('http://localhost:6120/api/drink/detail/',{
+                    params:{
+                        sn:Number(e.target.id),
+                    },
+                }).then(res=>{
+                    writeModal(res.data.detail);
+                })
+        }
+
+        if(modal.classList.contains("block") && !modal.contains(e.target)){
+            closeModal();
+        }
+    })
+
+
+    const writeModal = (data) => {
+        modalTitleKr.textContent = data.title;
+        modalTitleEn.textContent = data.englishTitle;
+        modalDescription.textContent = data.description;
+        cal.textContent = `(${data.calorie}kcal)`;
+        sugar.textContent = `(${data.sugars}g)`;
+        protein.textContent = `(${data.protein}g)`;
+        fat.textContent = `(${data.saturatedFat}g)`;
+        na.textContent = `(${data.natrium}mg)`;
+        caffeine.textContent = `(${data.caffeine}mg)`;
+        showModal();
     }
-})
 
 
-const writeModal = (data) => {
-    modalTitleKr.textContent = data.title;
-    modalTitleEn.textContent = data.englishTitle;
-    modalDescription.textContent = data.description;
-    cal.textContent = `(${data.calorie}kcal)`;
-    sugar.textContent = `(${data.sugars}g)`;
-    protein.textContent = `(${data.protein}g)`;
-    fat.textContent = `(${data.saturatedFat}g)`;
-    na.textContent = `(${data.natrium}g)`;
-    caffeine.textContent = `(${data.caffeine}g)`;
-    showModal();
-}
 
-const showModal = () => {
-    if(!modal.classList.contains("block")){
-        modal.className += " block";
-        modalOuter.className += " block";
-        body.className += "onmodal";
+    const setDrink = (drinks) => {
+        let drinksHtml = ''
+        drinks.forEach(
+            function(drink){
+                drinksHtml += `
+                    <div class="drink grid-item" id=${currentIndex}>
+                        <img class="drink img" id=${currentIndex} src=${drink.imgSrc}>
+                        <div class="drink drink-title" id=${currentIndex}>${drink.title}</div>
+                    </div>
+                `;
+                currentIndex += 1;
+            }
+        )
+        container.innerHTML += drinksHtml;
+        closeLoading();
+        loading = false;
     }
-}
 
-const closeModal = () => {
-    modal.classList.remove("block");
-    modalOuter.classList.remove("block");
-    body.classList.remove("onmodal");
-}
+    // const setEvent = () => {
+    //     console.log(document.querySelectorAll(".grid-item"));
+    // }
 
-const setDrink = (idx1,idx2) => {
-    console.log(drinks,idx1,idx2);
-    for (let i = idx1; i < idx1+idx2+1; i++) {
-        if(lastIdx && i === lastIdx)break;
-        container.innerHTML += `
-            <div class="drink grid-item" id=${i}>
-                <img class="drink img" id=${i} src=${drinks[i].imgSrc}>
-                <div class="drink drink-title" id=${i}>${drinks[i].title}</div>
-            </div>
-        `
-    }
-    loading = false;
+
 }
